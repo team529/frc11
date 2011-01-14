3160b8289980d53d7e95e9a91e538872e0b495c4
Zach Banks <zach@zbanks.net>
2011-01-13 23:23:36 -0500

    zach - feedback & CAN

--- src/edu/wpi/first/wpilibj/templates/Robot.java
+++ src/edu/wpi/first/wpilibj/templates/Robot.java
@@ -9,6 +9,7 @@ package edu.wpi.first.wpilibj.templates;
 
 
 import edu.wpi.first.wpilibj.ADXL345_I2C;
+import edu.wpi.first.wpilibj.CANJaguar;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -29,7 +30,8 @@ import edu.wpi.first.wpilibj.RobotDrive;
  */
 public class Robot extends IterativeRobot {
 
-
+    private CANJaguar cjagLeft;
+    private CANJaguar cjagRight;
     private Jaguar jagLeft;
     private Jaguar jagRight;
 
@@ -55,19 +57,53 @@ public class Robot extends IterativeRobot {
     private static final int kSlotDigital = 2;
     private static final int kSlotAnalog = 1;
     private static final int kSlotPneumatic = 7;
+    private static final boolean kUseCAN = true;
 
+    private static final int kEncCodesPerRev = 250;       // rev
     private static final double kEncDistPerPulse = 30.0;  // in
     private static final double kGyroSensitivity = 0.05;  // V/(rad/s)
 
     private static final boolean kInvertLineSensor = false; // Test dark line on white
     private static final double kDeadband = 0.2;
+    private static final double kMaxSpeed = 1000; // kEncCodesPerRev / min
+    private static final double kAutoSpeed = 0.4;
+    private static final double kAutoCurve = 0.5;
+   
 
     public void robotInit() {
 
-        jagLeft = new Jaguar(kSlotDigital, 1);
-        jagRight = new Jaguar(kSlotDigital, 2);
+        if(kUseCAN){
+            cjagLeft = new CANJaguar(10);
+            cjagRight = new CANJaguar(11);
 
-        drive = new RobotDrive(jagLeft, jagRight);
+            cjagLeft.changeControlMode(CANJaguar.ControlMode.kSpeed);
+            cjagRight.changeControlMode(CANJaguar.ControlMode.kSpeed);
+
+            cjagLeft.setSpeedReference(CANJaguar.SpeedReference.kQuadEncoder);
+            cjagRight.setSpeedReference(CANJaguar.SpeedReference.kQuadEncoder);
+
+            cjagLeft.configEncoderCodesPerRev(kEncCodesPerRev);
+            cjagRight.configEncoderCodesPerRev(kEncCodesPerRev);
+
+            cjagLeft.configNeutralMode(CANJaguar.NeutralMode.kBrake);
+            cjagRight.configNeutralMode(CANJaguar.NeutralMode.kBrake);
+
+            cjagLeft.setPID(0.08, 0.000001, 0.01); //TODO: tune
+            cjagRight.setPID(0.08, 0.000001, 0.01);
+
+            cjagLeft.enableControl();
+            cjagRight.enableControl();
+
+            drive = new RobotDrive(cjagLeft, cjagRight);
+            drive.setMaxOutput(kMaxSpeed);
+        }else{
+            jagLeft = new Jaguar(kSlotDigital, 1);
+            jagRight = new Jaguar(kSlotDigital, 2);
+            
+            drive = new RobotDrive(jagLeft, jagRight);
+        }
+
+        
 
         ds = DriverStation.getInstance();
         jsLeft = new Joystick(1);
@@ -119,7 +155,13 @@ public class Robot extends IterativeRobot {
     }
 
     public void autonomousPeriodic() {
-
+        if(lineLeft.get() ^ kInvertLineSensor){
+            drive.drive(kAutoSpeed, -kAutoCurve);
+        }else if(lineRight.get() ^ kInvertLineSensor){
+            drive.drive(kAutoSpeed, -kAutoCurve);
+        }else{
+            drive.drive(kAutoSpeed, 0.0);
+        }
     }
 
     public void teleopInit(){
@@ -132,15 +174,17 @@ public class Robot extends IterativeRobot {
         dir.setX(jsLeft.getX());
         dir.setY(jsLeft.getY());
         //dir.normalize();
-        dir.rotate(gyroXY.getAngle());
+        //dir.rotate(gyroXY.getAngle());
 
+        //System.out.println("Gyro" + gyroXY.getAngle());
         if(dir.getR2() > kDeadband){
             drive.arcadeDrive(dir.getX(), dir.getY());
         }else{
             //turnController.setSetpoint(gyroXY.pidGet());
             //turnController.disable();
             drive.stopMotor();
-
         }
+
+        
     }
 }
